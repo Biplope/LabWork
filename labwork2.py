76e3533e0564f6bfdb55b0a7e0f065837dc1ff34
Biplope <maharjanbiplope16@gmail.com>
2021-12-02 12:06:39 +0545

    homework

---
+++ labwork2.py
@@ -0,0 +1,10 @@
+#  area of triangle
+
+
+height = int(input("enter the height of triangle :  "))
+base = int(input("enter the base of triangle :  "))
+
+areaOfTriangle = (height * base) * 0.5
+
+# print(areaOfTrialgle)
+print(f"the area of triangle is {areaOfTriangle}  cm\u00b2")
